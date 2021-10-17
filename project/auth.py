97acd5ddf9cd0e5db8994849fc32e592e22962a3
Kenneth Jimenez Pluas <kjimenez@telconet.ec>
2021-10-17 13:06:04 -0500

    Signup with ajax

--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-from . import db
+from . import db 
+import time
 
 auth = Blueprint('auth', __name__)
 
@@ -54,6 +55,36 @@ def signup_post():
     db.session.commit()
 
     return redirect(url_for('auth.login'))
+
+@auth.route('/ajax_signup', methods=['POST'])
+def ajax_signup_post():
+    _json = request.json
+    responseMessage = {'message' : 'The account has been created'}
+    responseStatusCode = 200
+
+    time.sleep(5)
+	
+    email    = _json['email']
+    name     = _json['name']
+    password = _json['password']
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        responseMessage = {'message' : 'Email address already exists'}
+        responseStatusCode = 400
+    else:
+        # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+        new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+    
+    response = jsonify(responseMessage)
+    response.status_code = responseStatusCode
+
+    return response
 
 @auth.route('/logout')
 @login_required
